3fd452267dd525f55d61921642372c7113b72cfc
Isaac Wallace <iwallace96@gmail.com>
2021-12-17 20:49:36 -0500

    Added seperate file for part 2, cleaned up repetitive code.

--- finalProject1/part1.cs
+++ finalProject1/part1.cs
@@ -1,5 +1,6 @@
 ﻿namespace Part1
 {
+    using part2;
     using System;
     using System.Collections.Generic;
     using System.IO;
@@ -90,7 +91,7 @@
                 //   throw an exception if the state is not found.
                 if (!taxRecords.ContainsKey(userState))
                 {
-                    throw new Exception("Tax records does not contain the users inputted state {userState}\n");
+                    throw new Exception($"Tax records does not contain the users inputted state {userState}\n");
                 }
                 else
                 {
@@ -130,8 +131,10 @@
                                 Console.WriteLine($"(Your Income: {userIncome}  - Tax Floor: {list.floor}) * Tax Bracket Rate: {list.rate}" +
                                     $"\n Your running total: {finalTax}\n");
                             }
+                            
 
                         }
+                        
                     }
                     //User has selected NOT to receive a detailed report(Silent Mode)
                     else
@@ -156,14 +159,17 @@
                                 //        this number is the total final tax, and can be returned as the final answer
                                 finalTax += (userIncome - list.floor) * list.rate;
                             }
+                            
 
                         }
+                        
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Error at ComputeTaxFor {e}");
+                Console.WriteLine($"Error at ComputeTaxFor {e}\n" +
+                    $"-------------------------------------------------------------------");
             }
             
             return Math.Round((decimal)finalTax, 2);
@@ -241,160 +247,15 @@
     /// seperation for visibility
     /// ///////////////////////////////////////////////////////
     /// 
-    internal class Employee
-    {
-        // create a Employee class representing a line from the file.  It shoudl have public properties of the correct type
-        // for each of the columns in the file
-
-        public double iD;
-        public string name;
-        public string stateCode;
-        public double hrsWorked;   
-        public double rate;     
-        public double taxDue;
-        public double income;
-        // define a list of states to validate the employees state
-        List<string> states = new List<string>() {"AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", 
-            "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", 
-            "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", 
-            "SC", "SD", "TN", "TX", "UM", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"};
-        
-        //  Create a ctor taking a single string (a csv) and use it to load the record
-
-        public Employee(string data)
-        {
-            try
-            {
-                //split data and store in appropriate property name
-                string[] words;
-                words = data.Split(',');
-                
-                //check that the string is formatted correctly
-                if (words.Length != 5)
-                {
-                    throw new Exception("Employee data is not formatted correctly. Data must contain 5 cells seperated by ','");
-                    
-                }
-
-                this.iD = double.Parse(words[0]);
-                this.name = words[1];
-                this.stateCode = words[2];
-                this.hrsWorked = double.Parse(words[3]);
-                this.rate = double.Parse(words[4]);
-                this.income = hrsWorked * rate;
-
-                var detailedReport = false;
-                this.taxDue = (double)TaxCalculator.ComputeTaxFor(this.stateCode, this.income, detailedReport);
-                //check that employees state is valid
-                if (!states.Contains(this.stateCode))
-                {
-                    throw new Exception("Employees state not valid");
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"{e}\n");
-                this.iD = double.NaN;
-                this.name = null;
-                this.stateCode = null;
-                this.hrsWorked = double.NaN;
-                this.rate = double.NaN;
-                this.income = double.NaN;
-                this.taxDue = double.NaN;
-                
-
-            }
-        }
-
-        
-    }
-
-    /// seperation for visibility
-    /// ///////////////////////////////////////////////////////
-    /// 
-    internal static class EmployeeRecord
-    {
-        // Create a static dictionary field that holds a List of Employee and is keyed by a string
-
-        internal static Dictionary<string, List<Employee>>  employees= new Dictionary<string, List<Employee>>();
-
-        // create a static constructor that:
-
-         static EmployeeRecord()
-        {
-            // enter a try/catch block for the entire static constructor to print out a message if an error occurs
-
-            try
-            {
-
-                // declare a streamreader to read a file
-                StreamReader sr;
-                // initialize the static dictionary to a newly create empty one
-                employees = new Dictionary<string, List<Employee>>() { };
-                // open the taxtable.csv file into the streamreader
-
-
-                using (sr = new StreamReader(@"C:\Users\user\source\repos\finalProject1\finalProject1\employees.csv"))
-                {
-                    //Create a Variable to call ReadLine, each time you call ReadLine it will skip down to the next line.
-                    string myString;
-                    // loop over the lines from the streamreader
-                    // Read the stream as a string, and write the string to the console. 
-
-                    while ((myString = sr.ReadLine()) != null)
-                    {
-
-                        // constuct an instance of employee record from the (csv) line in the file
-                        Employee record = new Employee(myString);
-
-                        //if an error was found in myString, skip this line
-                        if (record.name == null)
-                        {
-                            Console.WriteLine("Line has been skipped\n" +
-                                "----------------------------------------------------------------------------------");
-                            continue;
-                        }
-                        else {
-                            //Add employee to our   list
-                            Console.WriteLine($"Employee: #{record.iD} {record.name}  validated, now adding records\n" +
-                                "------------------------------------------------------------------------------------------------------------");
-                            //employees[record.iD].Add(record);
-                            employees.Add(record.iD.ToString(), new List<Employee>() { record });
-
-
-                        }
-                            
-                        
-                    }
-                    //provide a way to get out of the loop when you are done with the file....
-                    // catch any exceptions while processing each line in another try/catch block located INSIDE the loop
-                    //   this way if the line in the CSV file is incorrect, you will continue to process the next line
-                    // make sure the streamreader is disposed no matter what happens
-                    sr.Dispose();
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error at stream reader \n {e}");
-            }
-
-            
-         }
-        
-        
-    }
-    /// seperation for visibility
-    /// ///////////////////////////////////////////////////////
-    /// 
-    internal class Program
+   
+    internal class program
     {
         
         public static void Main()
         {
              
             bool prompt = true;
+            
             // create an infinite loop to:
             while (prompt)
             {
@@ -410,32 +271,18 @@
                 }
                 Console.WriteLine("Please Enter an income");
                 // validate the data
-                double income;
-                try
-                {
-                    income = double.Parse(Console.ReadLine());
-                }
-                catch (Exception e)
-                {
-
-                    Console.WriteLine(e);
-                    continue;
-                }// <----------------------------------------------------------------------------------
-
+                bool incomeParse;
+                string userIncome = Console.ReadLine();
+                incomeParse = double.TryParse(userIncome, out double income);
                 //prompt user for verbose or silent mode, create a public variable to hold the value
-
                 Console.WriteLine("Do you wish to receive a detailed report of how your tax was calculated?(Y/N)");
                 var verbose = Console.ReadLine().ToUpper();
                 bool detailedReport;
-                if(verbose.Length != 1)
-                {
-                    Console.WriteLine("Your answer is invalid, this program will restart");
-                    continue;
-                }else if (verbose.Contains('Y'))
+                if (verbose.Contains('Y')  && verbose.Length == 1)
                 {
                     detailedReport= true;
                     
-                }else if (verbose.Contains('N'))
+                }else if (verbose.Contains('N') && verbose.Length == 1)
                 {
                     detailedReport = false;
                 }
@@ -446,97 +293,29 @@
                 }
                 
                 // calculate the tax due and print out the total
-                var userTaxDue = TaxCalculator.ComputeTaxFor(state, income, detailedReport);
-                Console.WriteLine($"Tax Due: {userTaxDue}");
-
+                var finalTax = TaxCalculator.ComputeTaxFor(state, income, detailedReport);
+                Console.WriteLine("Final Tax:" + finalTax);
                 //Check if user wants to see The Employee tax records
-                Console.WriteLine($"Do you wish to see a record of Employees? (Y/N)");
-                var showEmp = Console.ReadLine().ToUpper();
-                
-                if (showEmp.Length != 1 || showEmp.Contains("N"))
-                {
-                    Console.WriteLine("Invalid Entry...moving on....");
-                    prompt = false;
-                    continue;
-                }
-                else if (showEmp.Contains("Y"))
-                {
-                    
-                    foreach( var list in EmployeeRecord.employees)
-                    {
-                        foreach( var entry in list.Value)
-                        {
-                            Console.WriteLine($"Name:{entry.name} Tax Due:{entry.taxDue}");
-                        }
-                    }
-
-                }
+                Console.WriteLine($"Now Showing list of employees, please press any key to continue..");
+                Console.ReadLine();
+                var newEmp = new EmployeeRecord();
                 //Prompt user if they would like to sort the list of employees
-                Console.WriteLine($"If you would like to sort this list please enter one of the following choices..\n" +
-                    $"'STATE', 'INCOME', 'ID', 'NAME', 'TAX' other wise enter any key.");
+                Console.WriteLine($"If you would like view a sorted list of employees please enter one of the following choices..\n" +
+                    $"'STATE', 'INCOME', 'ID', 'NAME', 'TAX'\n other wise enter any key.");
                 var sort = Console.ReadLine().ToUpper();
+                
 
-                var employees = EmployeeRecord.employees;
-                List<Employee> sortedEmps = new List<Employee>();
 
-                //use linq to order our list of list
-                if (sort.Contains("STATE"))
+                foreach (Employee v in newEmp.SortedEmps(sort))
                 {
-                    foreach (var list in employees.Values)
-                    {
-                        sortedEmps.AddRange(list);
-                    }
-                    sortedEmps = sortedEmps.OrderBy(x => x.stateCode).ToList();
+                    Console.WriteLine($"ID: {v.iD} Name: {v.name} State: {v.stateCode} Income:{v.income} Tax Due: {v.taxDue}");
                 }
-                else if (sort.Contains("INCOME"))
-                {
-                    foreach (var list in employees.Values)
-                    {
-                        sortedEmps.AddRange(list);
-                    }
-                    sortedEmps = sortedEmps.OrderBy(x => x.income).ToList();
-                }
-                else if (sort.Contains("ID"))
-                {
-                    foreach (var list in employees.Values)
-                    {
-                        sortedEmps.AddRange(list);
-                    }
-                    sortedEmps = sortedEmps.OrderBy(x => x.iD).ToList();
-                }
-                else if (sort.Contains("NAME"))
-                {
-                    foreach (var list in employees.Values)
-                    {
-                        sortedEmps.AddRange(list);
-                    }
-                    sortedEmps = sortedEmps.OrderBy(x => x.name).ToList();
-                }
-                else if (sort.Contains("TAX"))
-                {
-                    foreach (var list in employees.Values)
-                    {
-                        sortedEmps.AddRange(list);
-                    }
-                    sortedEmps = sortedEmps.OrderBy(x => x.taxDue).ToList();
-                }
-                //print to console
-                foreach (Employee r in sortedEmps)
-                {   
-                        Console.WriteLine($"ID: {r.iD} Name: {r.name} State: {r.stateCode} Income:{r.income} Tax Due: {r.taxDue}"); 
-                }
-
 
                 //prompt user for another tax record
                 Console.WriteLine($"Do you want to check another tax record? (Y/N)");
                 var loopAgain = Console.ReadLine().ToUpper();
-                if(loopAgain.Length !=1)
-                {
-                    Console.WriteLine("Answer Invalid This program will end..");
-                    prompt = false;
-                    break;
-                }
-                if (loopAgain.Contains("Y"))
+                
+                if (loopAgain.Contains("Y") && loopAgain.Length == 1)
                 {
                     prompt = true;
                 }else if (loopAgain.Contains("N"))
